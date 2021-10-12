db9fdb02751c9859a32c2dbb8e78d062a73b7f9d
pandersnoh <spandernoh@gmail.com>
2021-10-13 02:17:30 +0900

    Modify Router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HelloWorld from '~/components/HelloWorld.vue'
-import Example from '~/components/Example.vue'
 import TodoIndex from '~/components/TodoIndex.vue'
 
 export default createRouter({
@@ -8,18 +6,8 @@ export default createRouter({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: HelloWorld
-    },
-    {
-      path: '/example',
-      name: 'Example',
-      component: Example
-    },
-    {
-      path: '/todos',
       name: 'TodoIndex',
       component: TodoIndex
-    },
+    }
   ]
 })
